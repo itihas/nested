10c3b4ba1f4735d206a993255cf4e350651abea7
Sahiti <sahiti93@gmail.com>
2018-02-03 11:15:05 +0530

    a Game is now either and Outcome or a Game

--- src/Lib.hs
+++ src/Lib.hs
@@ -12,13 +12,11 @@ newtype Player = Player {player::Int} deriving (Show, Eq)
 
 newtype State = State String deriving (Show, Eq)
   
-newtype Outcome = Outcome String deriving (Show, Eq)
-
-
-data Game = AtomGame {players :: Int
+data Game = Outcome String
+          | Game {players :: Int
                  , states :: [State]
-                 , outcomes :: [Outcome]
-                 , label:: State -> Outcome -- s in states, o in outcomes
+                 , outcomes :: [Game]
+                 , label:: State -> Game -- s in states, o in outcomes
                  , playerMoves :: Player -> State -> Int -- player(p) < players,s in states, i represents number of actions that can be taken
                  , transition :: State -> [Int] -> State    -- s in states, length([i]) == players, [i]!!n < playerMoves(n, s)
                  }
@@ -26,7 +24,7 @@ data Game = AtomGame {players :: Int
           -- TODO: lots of checks to obey the rules in the comments above
           
 instance Show Game where
-  show g@(AtomGame _ _ _ _ _ _) = (show $ players g) ++ ", " ++ (show $ states g) ++ ", " ++ (show $ outcomes g)
+  show g@(Game _ _ _ _ _ _) = (show $ players g) ++ ", " ++ (show $ states g) ++ ", " ++ (show $ outcomes g)
 
 
 
@@ -39,7 +37,7 @@ instance Show Game where
 --             }
 
 fptpVote :: Int -> Int -> Game
-fptpVote k a = AtomGame { players = k
+fptpVote k a = Game { players = k
                      , states = [State $ show i | i <- [0..a]]
                      , outcomes = [Outcome $ show i | i <- [1..a]]
                      , label = \(State i) -> Outcome i
@@ -56,7 +54,7 @@ fptpVote k a = AtomGame { players = k
                   -- I wonder if it can be expressed as the composition of last-past-the post eliminations?
 
 lptpElimination :: Int -> Int -> Game
-lptpElimination k a = AtomGame {players = k
+lptpElimination k a = Game {players = k
                            , states = [State $ show i | i <- [0..a]]
                            , outcomes = [Outcome $ show i | i <- [1..a]]
                            , label = \(State i) -> Outcome i
@@ -69,7 +67,7 @@ lptpElimination k a = AtomGame {players = k
                            }
 
 alternativeVote :: Int -> Int -> Game
-alternativeVote k a = AtomGame { players = k
+alternativeVote k a = Game { players = k
                      , states = [State $  show i | i <- [0..a]]
                      , outcomes = [Outcome $ show i | i <- [1..a]]
                      , label = \(State i) -> Outcome i
