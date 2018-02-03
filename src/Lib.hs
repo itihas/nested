a2094cc632e7a0c6e6a1a7e73db9f54318892696
Sahiti <sahiti93@gmail.com>
2018-02-03 10:21:33 +0530

    structure of alternativeVote. Musings on composing Games.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,7 +1,7 @@
 module Lib where
 
 import qualified Data.Set as Set
-import Data.List (sort, group, maximumBy)
+import Data.List (sort, group, maximumBy, minimumBy)
 import Data.Ord (comparing)
 
 someFunc :: IO ()
@@ -10,52 +10,70 @@ someFunc = putStrLn "someFunc"
 
 newtype Player = Player {player::Int} deriving (Show, Eq)
 
-newtype State = State Int deriving (Show, Eq)
+newtype State = State String deriving (Show, Eq)
   
-newtype Outcome = Outcome Int deriving (Show, Eq)
+newtype Outcome = Outcome String deriving (Show, Eq)
 
 
-data Game = Game {players :: Int
+data Game = AtomGame {players :: Int
                  , states :: [State]
                  , outcomes :: [Outcome]
                  , label:: State -> Outcome -- s in states, o in outcomes
                  , playerMoves :: Player -> State -> Int -- player(p) < players,s in states, i represents number of actions that can be taken
                  , transition :: State -> [Int] -> State    -- s in states, length([i]) == players, [i]!!n < playerMoves(n, s)
-                  } 
+                 }
 
           -- TODO: lots of checks to obey the rules in the comments above
           
 instance Show Game where
-  show g = (show $ players test) ++ ", " ++ (show $ states test) ++ ", " ++ (show $ outcomes test)
-
-test = Game { players = 3
-            , states = [State 1, State 2, State 3]
-            , outcomes =  [Outcome 1, Outcome 2, Outcome 3]
-            , label = \(State i) -> Outcome i
-            , playerMoves = \(Player p) (State s) -> p*s
-            , transition = \(State s) as -> State (((product as) * s) `mod` 3)
-            }
+  show g@(AtomGame _ _ _ _ _ _) = (show $ players g) ++ ", " ++ (show $ states g) ++ ", " ++ (show $ outcomes g)
 
 
-fptpVote k a = Game { players = k
-                     , states = [State i | i <- [0..a]]
-                     , outcomes = [Outcome i | i <- [1..a]]
+
+-- test = Game { players = 3
+--             , states = [State 1, State 2, State 3]
+--             , outcomes =  [Outcome 1, Outcome 2, Outcome 3]
+--             , label = \(State i) -> Outcome i
+--             , playerMoves = \(Player p) (State s) -> p*s
+--             , transition = \(State s) as -> State (((product as) * s) `mod` 3)
+--             }
+
+fptpVote :: Int -> Int -> Game
+fptpVote k a = AtomGame { players = k
+                     , states = [State $ show i | i <- [0..a]]
+                     , outcomes = [Outcome $ show i | i <- [1..a]]
                      , label = \(State i) -> Outcome i
-                     , playerMoves = \(Player p) (State s) -> if s==0
+                     , playerMoves = \(Player p) (State s) -> if s=="0"
                                                               then 3
                                                               else 0
-                     , transition = \(State s) js -> if s==0
-                                                     then State ((maximumBy (comparing length) . group $ sort js)!!0) -- find the mode of player actions
+                     , transition = \(State s) js -> if s=="0"
+                                                     then State $ show ((maximumBy (comparing length) . group $ sort js)!!0) -- find the mode of player actions
                                                      else State s
                  }
 
 
+                  -- alternative vote has n levels of m intermediate states each where players express their preferences among the m remaining candidates. It's like iterated last-past-the-post-elimination.
+                  -- I wonder if it can be expressed as the composition of last-past-the post eliminations?
 
-alternativeVote k a = Game { players = k
-                     , states = [State i | i <- [0..a]]
-                     , outcomes = [Outcome i | i <- [1..a]]
+lptpElimination :: Int -> Int -> Game
+lptpElimination k a = AtomGame {players = k
+                           , states = [State $ show i | i <- [0..a]]
+                           , outcomes = [Outcome $ show i | i <- [1..a]]
+                           , label = \(State i) -> Outcome i
+                           , playerMoves = \(Player p) (State s) -> if s=="0"
+                                                                    then 3
+                                                                    else 0
+                           , transition = \(State s) js -> if s=="0"
+                                                           then State $ show ((minimumBy (comparing length) . group $ sort js)!!0) -- find the anti-mode of player actions
+                                                           else State s
+                           }
+
+alternativeVote :: Int -> Int -> Game
+alternativeVote k a = AtomGame { players = k
+                     , states = [State $  show i | i <- [0..a]]
+                     , outcomes = [Outcome $ show i | i <- [1..a]]
                      , label = \(State i) -> Outcome i
-                     , playerMoves = \(Player p) (State s) -> if s==0
+                     , playerMoves = \(Player p) (State s) -> if s=="0"
                                                               then 3
                                                               else 0
                      , transition = \(State s) js -> undefined -- TODO
